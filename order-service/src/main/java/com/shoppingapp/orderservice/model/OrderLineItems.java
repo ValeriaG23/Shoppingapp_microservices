2b724b493a60de5242938b32e39885b66ff3bf86
ValeriaG23 <valeria.guranda@isa.utm.md>
2024-05-20 15:29:37 +0300

    add Order class and OrderLineItems class in model package

---
+++ order-service/src/main/java/com/shoppingapp/orderservice/model/OrderLineItems.java
@@ -0,0 +1,23 @@
+package main.java.com.shoppingapp.orderservice.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity //pt tabel
+@Table(name = "t_order_line_items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class OrderLineItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
